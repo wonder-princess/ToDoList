a0c8c094349f07bad0483dd065abc8a9dcf9fb1a
wonder_princess <sekai_love.me.do@hotmail.co.jp>
2019-10-08 01:24:24 +0900

    todolist_ver2

--- ToDoList/app/src/main/java/com/example/todolist/InputDIalog.kt
+++ ToDoList/app/src/main/java/com/example/todolist/InputDIalog.kt
@@ -1,36 +1,35 @@
 package com.example.todolist
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
-class InputDIalog : DialogFragment(){
+class InputDialog : DialogFragment(){
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
-
+//        var inputText = findViewById<TextView>(R.id.input_text)
         builder.setTitle(R.string.dialog_title)
-        builder.dialogTextData = input_text.toString()
         builder.setPositiveButton(R.string.dialog_btn_submit, DialogButtonClickListener())
         builder.setNegativeButton(R.string.dialog_btn_cancel,DialogButtonClickListener())
-
-
         val dialog = builder.create()
         return dialog
     }
 
     private inner class DialogButtonClickListener : DialogInterface.OnClickListener{
+        @SuppressLint("ResourceType")
         override fun onClick(dialog: DialogInterface, which: Int){
+            var foo = ""
             when(which){
                 DialogInterface.BUTTON_POSITIVE ->
-                    .setText(R.id.input_text)
-
-                DialogInterface.BUTTON_NEGATIVE ->
-                    msg = "キャンセル"
+                    foo = getString(R.id.input_text)
+//                    items.add(findViewById<TextView>(R.id.input_text).text.toString())
+                DialogInterface.BUTTON_POSITIVE ->
+                    foo = ""
+//                    inputText.setText("")
             }
 
         }
--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.todolist
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
+import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +19,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list_value = findViewById<ListView>(R.id.list_value)
-        val items =  Array(10, {i -> ""})
+        val listValue = findViewById<ListView>(R.id.list_value)
+        val items = ArrayList<String>()
         val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_expandable_list_item_1, items)
-        list_value.adapter = adapter
+        listValue.adapter = adapter
+        listValue.onItemClickListener = LIstItemClickListener()
 
-        list_value.onItemClickListener = LIstItemClickListener()
+        val addButton = findViewById<Button>(R.id.add_button)
+        addButton.setOnClickListener(AddButtonClickListener())
+    }
+    private inner class AddButtonClickListener : View.OnClickListener{
+        override fun onClick(view: View) {
+
+//            val dialog = InputDialog()
+//            dialog.show(supportFragmentManager, "OrderConfirmDialogFragment")
+        }
     }
     private inner class LIstItemClickListener : AdapterView.OnItemClickListener{
         override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-            val dialog = InputDIalog()
-
-            dialog.show(supportFragmentManager, "OrderConfirmDialogFragment")
+//            val dialog = EditDialog()
+//            dialog.show(supportFragmentManager, "OrderConfirmDialogFragment")
         }
     }
-
 }
